8c5ffcfe2e2e78b14cf6ced0effd4fc6f6bc2f27
danieltadresu <danieldev1999@gmail.com>
2022-05-16 22:51:21 -0400

    feat(:construction): add main content

--- academy-web/src/App.js
+++ academy-web/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.Body}>
+      <header className={classes.Header}>
+        Header
       </header>
+      <main className={classes.Main}>
+        <div className={classes.Box}>
+          <h1 className={classes.Title}>
+            Hey, I am Daniel! Full-stack web and mobile developer, from Chile
+          </h1>
+        </div>
+        <div className={classes.Box}>
+          <h3 className={classes.SubTitle}>
+            I am passionate about development
+            — I love automating things, modern technologies and good developer experiencie.
+          </h3>
+          <h3 className={classes.SubTitle}>
+            I am a perfectionist. I pay attention to details,
+            because I love when things look good and professionnal.
+            I am also self-taught, and I never stop learning.
+          </h3>
+        </div>
+      </main>
     </div>
   );
 }
