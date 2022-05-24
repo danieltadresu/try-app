f2c13fd10722b78e5486e0d3f1578df63c87421d
danieltadresu <danieldev1999@gmail.com>
2022-05-23 23:00:11 -0400

    :sparkles: improve Tech section icons

--- academy-web/src/components/Stack.js
+++ academy-web/src/components/Stack.js
@@ -1,12 +1,15 @@
 import classes from './Stack.module.css';
-import { SiExpress, SiPostgresql } from 'react-icons/si';
+import { SiExpress, SiPostgresql, SiSequelize } from 'react-icons/si';
 import { FaReact, FaAws } from 'react-icons/fa';
 import { AiFillHtml5 } from 'react-icons/ai';
 import { DiCss3 } from 'react-icons/di';
+import { BsFillTerminalFill } from 'react-icons/bs';
+import { DiDjango, DiNodejs, DiMongodb } from 'react-icons/di';
+import { SiMongodb, SiNextdotjs } from 'react-icons/si';
 
 function Stack() {
   return (
-    <main className={classes.Box}>
+    <main className={classes.MainContainer}>
       <section>
         <div className={classes.Top}>
           <h1>Stack</h1>
@@ -19,7 +22,7 @@ function Stack() {
       </section>
 
       {/* TECH ICONS */}
-      <section className={classes.IconsContainer}>
+      <section className={classes.SecondContainer}>
         <div className={classes.ItemIcon}>
           <AiFillHtml5 className={classes.Icon} />
         </div>
@@ -37,6 +40,60 @@ function Stack() {
         </div>
         <div className={classes.ItemIcon}>
           <FaAws className={classes.Icon} />
+        </div>
+      </section>
+
+      <section className={classes.ThirdContainer}>
+        <h1>Other technologies I work with</h1>
+        <h3>Here is a list of other tools I am familiar with.</h3>
+        <div className={classes.BackendTechnologies}>
+          <div className={classes.Box}>
+            <span>BACKEND</span>
+          </div>
+        </div>
+      </section>
+
+      <section className={classes.FourthContainer}>
+        <div className={classes.ItemIcon}>
+          <SiNextdotjs className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <DiNodejs className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <SiSequelize className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <DiDjango className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <SiMongodb className={classes.Icon} />
+        </div>
+      </section>
+
+      <section className={classes.FifthContainer}>
+        <div className={classes.BackendTechnologies}>
+          <div className={classes.Box}>
+            <span>FRONTEND</span>
+          </div>
+        </div>
+      </section>
+
+      <section className={classes.FourthContainer}>
+        <div className={classes.ItemIcon}>
+          <SiNextdotjs className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <DiNodejs className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <SiSequelize className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <DiDjango className={classes.Icon} />
+        </div>
+        <div className={classes.ItemIcon}>
+          <SiMongodb className={classes.Icon} />
         </div>
       </section>
     </main>
